2b4812ed52cf4174781d732c422796abb73529a9
Logan Karnes <logan.karnes98@gmail.com>
2019-06-13 19:12:25 -0700

    k

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -12,25 +12,25 @@ class TabLink {
  
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
-    ;
+    
     // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener('click', this.tabItem.select)
+    this.element.addEventListener('click',()=> this.tabItem.select());
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links = document.querySelector('.tabs-link');
+    const links = document.querySelectorAll('.tabs-link');
     
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(tab, ()=>{
-      this.links.classList.remove('tabs-link-selected');
+    Array.from(links).forEach(tab=>{
+      tab.classList.remove('tabs-link-selected');
     });
 
     // Add a class named "tabs-link-selected" to this link
     this.element.classlist.add("tabs-link-selected");
   
     // Call the select method on the item associated with this link
-    this.tabItem.select;
+    this.tabItem.select();
   }
 }
 
@@ -38,20 +38,18 @@ class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
     this.element = element;
-    console.log(this.element.classList);
+    console.log(this.element);
   }
-
   select() {
+    console.log(this.element)
     // Select all ".tabs-item" elements from the DOM
     const items = document.querySelectorAll(".tabs-item");
     // Remove the class "tabs-item-selected" from each element
-    items.forEach(elements =>{
+    Array.from(items).forEach(elements =>{
       elements.classList.remove('tabs-link-selected');
     });
-    
     // Add a class named "tabs-item-selected" to this element
     this.element.classList.add('tabs-item-selected');
-    
   }
 }
 
